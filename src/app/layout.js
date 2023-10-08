bcc665a57622938e8151634f0df34da98b3df38a
Josh Nguyen <joshuahnguyen@gmail.com>
2023-10-07 17:05:21 -0700

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,6 @@ export const metadata = {
   title: "Joshua Nguyen's Website",
   description: '',
 }
-
 const nunito = Nunito({
 subsets: ['latin'],
 variable: '--font-nunito'
@@ -17,6 +16,7 @@ variable: '--font-nunito'
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${nunito.variable} font-sans scroll-smooth`}>
+      <link rel='icon' href='/favicon/favicon.ico'/>
       <body>{children}</body>
     </html>
   )
