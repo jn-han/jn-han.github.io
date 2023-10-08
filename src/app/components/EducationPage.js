9a0e71d967a7a64b7ba1c6e11126a598f898fb20
Joshua Nguyen <joshuahnguyen@gmail.com>
2023-10-08 13:00:52 -0700

    added padding for mobile

--- src/app/components/EducationPage.js
+++ src/app/components/EducationPage.js
@@ -10,7 +10,7 @@ const EducationPage = () => {
       id="education"
       className="flex min-h-screen w-full flex-col items-center justify-between"
     >
-      <div className="mt-24 flex flex-col justify-center items-center text-green md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-1/2">
+      <div className="mt-24 p-5 md:p-0 lg:p-0 xl:p-0 2xl:p-0 flex flex-col justify-center items-center text-green md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-1/2">
         <h2 className="text-green text-3xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-5xl">
           My Education
         </h2>
