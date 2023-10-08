115443784070d206dde2f562c6b6bc814bfe88bd
Josh Nguyen <joshuahnguyen@gmail.com>
2023-10-07 23:15:57 -0700

    added additional scaling fixes to other pages for mobile

--- src/app/components/EducationPage.js
+++ src/app/components/EducationPage.js
@@ -11,7 +11,9 @@ const EducationPage = () => {
       className="flex min-h-screen w-full flex-col items-center justify-between"
     >
       <div className="mt-24 flex flex-col justify-center items-center text-green md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-1/2">
-        <h2 className="text-green text-5xl">My Education</h2>
+        <h2 className="text-green text-3xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-5xl">
+          My Education
+        </h2>
         <FontAwesomeIcon className="w-4 pt-10 " icon={faGripLinesVertical} />
         {EDUCATION.map((items, key) => (
           <EducationCard info={items} key={key} />
--- src/app/components/ProjectPage.js
+++ src/app/components/ProjectPage.js
@@ -11,7 +11,9 @@ const ProjectPage = () => {
       className="flex min-h-screen w-full flex-col items-center justify-between"
     >
       <div className="mt-24 flex flex-col justify-center items-center text-green md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-1/2">
-        <h2 className="text-green text-5xl">My Projects</h2>
+        <h2 className="text-green text-3xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-5xl">
+          My Projects
+        </h2>
         <FontAwesomeIcon className="w-4 pt-10 " icon={faGripLinesVertical} />
 
         {PROJECTS.map((item) => (
