cd7ca5f8d6d9e408639551e0751806221c59ad83
Joshua Nguyen <joshuahnguyen@gmail.com>
2023-10-08 16:56:52 -0700

    small css tweaks

--- src/app/components/ProjectPage.js
+++ src/app/components/ProjectPage.js
@@ -15,10 +15,13 @@ const ProjectPage = () => {
       className="flex min-h-screen w-full flex-col items-center justify-between"
     >
       <div className="mt-24 p-5 md:p-0 lg:p-0 xl:p-0 2xl:p-0 flex flex-col justify-center items-center text-green w-full md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-1/2">
-        <h2 className="text-green text-3xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-5xl">
+        <h2 className="text-green text-3xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-5xl border-b-2 w-full text-center border-accent pb-5">
           My Projects
         </h2>
-        <FontAwesomeIcon className="w-4 pt-10 " icon={faGripLinesVertical} />
+        <FontAwesomeIcon
+          className="pt-10 text-accent"
+          icon={faGripLinesVertical}
+        />
 
         {PROJECTS.map((item) => (
           <ProjectCard info={item} key={item.name} />
--- src/app/config/projects-config.js
+++ src/app/config/projects-config.js
@@ -4,7 +4,7 @@ const PROJECTS = [
     date: "Sept 2023",
     place: "Hack the North",
     description:
-      "Plotgen drew inspiration from the storytellers and artists who excite, move and thrill us with the magic that can be found in their work. We wanted to empower others with the same kind of magic that is now realizable with AI developments like LLMs and Diffusion Models. Seeing how new tools from companies like BentoML and Cohere make it easy for developers to build products from these developments, we knew we had to do something in the space of generative AI.",
+      "'plotgen' drew inspiration from the storytellers and artists who excite, move and thrill us with the magic that can be found in their work. We wanted to empower others with the same kind of magic that is now realizable with AI developments like LLMs and Diffusion Models. Seeing how new tools from companies like BentoML and Cohere make it easy for developers to build products from these developments, we knew we had to do something in the space of generative AI.",
     award: '"Best Project Using BentoML"',
     builtWith: [
       "Next.js",
@@ -21,8 +21,8 @@ const PROJECTS = [
     date: "Feb 2023",
     place: "NwHacks",
     description:
-      "Our web-application was built using React, utilizing React Router to navigate through webpages, and Livepeer's API to allow users to upload content and host livestreams on our videos. For background context, Livepeer describes themselves as a decentralized video infracstucture network.",
-    award: '"Best Video Hack"',
+      "'kizuna' is a web application developed for communities as a solution to the lack of audience engagement features in common streaming platforms such as YouTube and Twitch. Using both Livepeer and Lens Protocol, our goal was to bring the lens community together to stream, watch, and palyback video on a decentralilzed network. ",
+    award: '"Best Video Application"',
     builtWith: ["React.JS", "CSS", "Livepeer", "Lens Protocol"],
     src: "/kizuna.png",
     link: "https://devpost.com/software/kizuna-gevr8p",
@@ -32,7 +32,7 @@ const PROJECTS = [
     date: "Aug 2023",
     place: "Ignition Hacks",
     description:
-      "inteflow is an iOS mobile app designed to streamline your math problem-solving experience. It's your personal problem-solving companion that scans both formula and text-based math problems. Once scanned, inteflow breaks down these problems, offering detailed explanations that guide you to the solution through a chatbot. It also offers a wide range of supplementary resources, curated practice problems and textbooks to aid in reinforcing your understanding.",
+      "'inteflow' is an iOS mobile app designed to streamline your math problem-solving experience. It's your personal problem-solving companion that scans both formula and text-based math problems. Once scanned, inteflow breaks down these problems, offering detailed explanations that guide you to the solution through a chatbot. It also offers a wide range of supplementary resources, curated practice problems and textbooks to aid in reinforcing your understanding.",
     award: "Finalist",
     builtWith: ["Swift", "SwiftUI", "Xcode", "Open AI"],
     src: "/inteflow.png",
