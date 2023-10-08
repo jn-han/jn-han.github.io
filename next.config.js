6e1f10d63f61d3bc2675341fb47d685270e9aa5a
Josh Nguyen <joshuahnguyen@gmail.com>
2023-10-07 23:48:38 -0700

    additional config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require("dotenv");
+dotenv.config();
+
 const nextConfig = {
-  output: "export",
+  // output: "export",
 };
 
 module.exports = nextConfig;
