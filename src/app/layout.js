c4810df495850816f3eb48bd4570dded4eb6e801
Joshua Nguyen <joshuahnguyen@gmail.com>
2025-02-09 21:48:38 -0800

    Implemented Open Graph Protocol Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,12 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 export const metadata = {
   title: "Joshua Nguyen's Website",
-  description: "",
-  openGraph: {
-    title: "Joshua Nguyen's Website",
-    description: "Explore my projects and blog posts.",
-    url: "https://joshuanguyen.ca/",
-    images: [
-      {
-        url: "/assets/web_preview.png",
-        width: 1200,
-        height: 630,
-        alt: "Joshua Nguyen's Website Preview",
-      },
-    ],
-  },
+  description: "Explore my projects and blog posts.",
 };
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -29,7 +18,22 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${inter.variable} font-sans font-light scroll-smooth`}
     >
-      <link rel="icon" href="/favicon/favicon.ico" />
+      <Head>
+        <meta property="og:title" content="Joshua Nguyen's Website" />
+        <meta
+          property="og:description"
+          content="Explore my projects and blog posts."
+        />
+        <meta property="og:url" content="https://joshuanguyen.ca/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://joshuanguyen.ca/web_preview.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </Head>
       <body>{children}</body>
     </html>
   );
