d14bf23d43b83c9ab3f978dfb28d9312d2f91767
Joshua Nguyen <joshuahnguyen@gmail.com>
2025-02-09 21:54:32 -0800

    bug fixes on preview

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,23 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 export const metadata = {
   title: "Joshua Nguyen's Website",
   description: "Explore my projects and blog posts.",
+  openGraph: {
+    title: "Joshua Nguyen's Website",
+    description: "Explore my projects and blog posts.",
+    url: "https://joshuanguyen.ca/",
+    images: [
+      {
+        url: "https://joshuanguyen.ca/assets/web_preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Joshua Nguyen's Website Preview",
+      },
+    ],
+    type: "website",
+  },
 };
 
 const inter = Inter({
@@ -18,22 +31,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${inter.variable} font-sans font-light scroll-smooth`}
     >
-      <Head>
-        <meta property="og:title" content="Joshua Nguyen's Website" />
-        <meta
-          property="og:description"
-          content="Explore my projects and blog posts."
-        />
-        <meta property="og:url" content="https://joshuanguyen.ca/" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://joshuanguyen.ca/web_preview.png"
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <link rel="icon" href="/favicon/favicon.ico" />
-      </Head>
+      <link rel="icon" href="/favicon/favicon.ico" />
       <body>{children}</body>
     </html>
   );
